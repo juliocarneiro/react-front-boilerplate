c1cf681c9b76f3ea79776006e525bbb5713ed8c1
juliocarneiro <juliocarneiro@gmail.com>
2017-10-01 18:58:30 -0300

    loader

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-
-//ROUTES
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 //ASSETS
@@ -13,18 +11,32 @@ import MainPage from './components/pages/MainPage';
 import Contact from './components/pages/Contact';
 
 class App extends Component {
-  render() {
-    return (
-        <Router>
-            <div>
-                <Header text="Funcionou!!!" icon="fa-hand-rock-o" />
-                    <Route exact path='/' component={MainPage} />
-                    <Route exact path='/contact' component={Contact} />
-                <Footer copy="Júlio Carneiro" year="2018" />
-            </div>
-        </Router>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+          loading: true
+        };
+    }
+    componentDidMount() {
+        setTimeout(() => this.setState({ loading: false }), 1500); 
+    }
+    render() {
+        const { loading } = this.state;
+    
+        if(loading) {
+            return null;
+        }
+        return (
+            <Router>
+                <div>
+                    <Header text="Funcionou!!!" icon="fa-hand-rock-o" />
+                        <Route exact path='/' component={MainPage} />
+                        <Route exact path='/contact' component={Contact} />
+                    <Footer copy="Júlio Carneiro" year="2018" />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
